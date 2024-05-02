df16940acd812a49cb42f034075b8256896b8c04
Sebastian Jensen <148753669+SebastianFJensen@users.noreply.github.com>
2024-05-02 14:12:31 +0200

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views 
 from .views import AddCommentView, import_from_excel, create_new_folder, upload_file, EditCommentView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+def azure_media_serve(request, path, document_root=None, show_indexes=False):
+    return serve(request, path, document_root=document_root, show_indexes=show_indexes)
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -24,4 +27,5 @@ urlpatterns = [
     path('record/<int:record_id>/comment/<int:comment_id>/edit/', views.EditCommentView.as_view(), name='edit_comment'),
     path('search/', views.search, name='search'),
     path('delete_file/<int:pk>/', views.delete_file, name='delete_file'),
+    re_path(r'^media/(?P<path>.*)$', azure_media_serve, {'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
